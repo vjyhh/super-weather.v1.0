298323df0de170f8f731b01342cc90d812557bc1
Vitaly <vitalyryabchich@gmail.com>
2022-08-18 15:55:11 +0300

    New Logic Axios.get

--- src/App.js
+++ src/App.js
@@ -1,34 +1,56 @@
 import axios from "axios";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [cityName, setCityName] = useState("");
+  const [temp, setTemp] = useState();
+  const [humidity, setHumidity] = useState();
+  const [wind, setWind] = useState();
+  const [inputCity, setInputCity] = useState();
+  const [isHover, setIsHover] = useState(false);
+
   let apiKey = "2e0076240708b84af82214022fa3d888";
 
-  let city = "Moscow";
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&units=metric&appid=${apiKey}`;
-
-  axios.get(url).then((res) => {
-    document.querySelector(".city").innerHTML = res.data.name;
-
-    document.querySelector(".temp").innerHTML = res.data.main.temp;
-
-    document.querySelector(".humidity").innerHTML = res.data.main.humidity;
-    document.querySelector(".wind").innerHTML = res.data.wind.speed;
-  });
-
   return (
-    <div class="weather">
+    <div className="weather">
+      <div className="searchCity">
+        <input
+          type="text"
+          id="name"
+          className="inputCityName"
+          onChange={(text) => {
+            setInputCity(text.target.value);
+          }}
+        />
+
+        <button
+          className="searchButton"
+          onClick={() => {
+            let url = `http://api.openweathermap.org/data/2.5/weather?q=${inputCity}&lang=ru&units=metric&appid=${apiKey}`;
+            axios.get(url).then((res) => {
+              setCityName(res.data.name);
+              setTemp(res.data.main.temp);
+              setHumidity(res.data.main.humidity);
+              setWind(res.data.wind.speed);
+            });
+            console.log(cityName);
+          }}
+        >
+          Search
+        </button>
+      </div>
       <h2>
-        Погода в городе <span class="city"></span>
+        Погода в городе <span className="city">{cityName}</span>
       </h2>
       <p>
-        Погода: <span class="temp"></span>°C
+        Погода: <span className="temp">{temp}</span>°C
       </p>
       <p>
-        Влажность: <span class="humidity"></span>%
+        Влажность: <span className="humidity">{humidity}</span>%
       </p>
       <p>
-        Ветер: <span class="wind"></span> км/ч
+        Ветер: <span className="wind">{wind}</span> км/ч
       </p>
     </div>
   );
