6e9cabd4dd6d41031b8895c65a612ddc30d191f1
Vitaly <vitalyryabchich@gmail.com>
2022-08-18 19:57:46 +0300

    header-menu

--- src/App.js
+++ src/App.js
@@ -8,38 +8,40 @@ function App() {
   const [humidity, setHumidity] = useState();
   const [wind, setWind] = useState();
   const [inputCity, setInputCity] = useState();
-  const [isHover, setIsHover] = useState(false);
 
   let apiKey = "2e0076240708b84af82214022fa3d888";
 
   return (
     <div className="weather">
-      <div className="searchCity">
-        <input
-          type="text"
-          id="name"
-          className="inputCityName"
-          onChange={(text) => {
-            setInputCity(text.target.value);
-          }}
-        />
+      <div className="headerMenu">
+        <div className="searchCity">
+          <input
+            type="text"
+            id="name"
+            className="inputCityName"
+            onChange={(text) => {
+              setInputCity(text.target.value);
+            }}
+          />
 
-        <button
-          className="searchButton"
-          onClick={() => {
-            let url = `http://api.openweathermap.org/data/2.5/weather?q=${inputCity}&lang=ru&units=metric&appid=${apiKey}`;
-            axios.get(url).then((res) => {
-              setCityName(res.data.name);
-              setTemp(res.data.main.temp);
-              setHumidity(res.data.main.humidity);
-              setWind(res.data.wind.speed);
-            });
-            console.log(cityName);
-          }}
-        >
-          Search
-        </button>
+          <button
+            className="searchButton"
+            onClick={() => {
+              let url = `http://api.openweathermap.org/data/2.5/weather?q=${inputCity}&lang=ru&units=metric&appid=${apiKey}`;
+              axios.get(url).then((res) => {
+                setCityName(res.data.name);
+                setTemp(res.data.main.temp);
+                setHumidity(res.data.main.humidity);
+                setWind(res.data.wind.speed);
+              });
+              console.log(cityName);
+            }}
+          >
+            Search
+          </button>
+        </div>
       </div>
+
       <h2>
         Погода в городе <span className="city">{cityName}</span>
       </h2>
